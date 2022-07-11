b55fc9791ed8137eb4b5bb5dabad515cf67b0eaa
Alex Lin <349461694@qq.com>
2022-07-11 14:26:24 -0400

    update
    finish vectorization part

--- assignments/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments/assignment1/cs231n/classifiers/linear_svm.py
@@ -37,6 +37,10 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1  # note delta = 1 
             if margin > 0:
                 loss += margin
+                
+                dW[:,j] += X[i]
+
+                dW[:,y[i]] -= X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -54,7 +58,9 @@ def svm_loss_naive(W, X, y, reg):
     # code above to compute the gradient.                                       #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-                
+    
+    dW /= num_train
+    dW += 2*reg*W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -76,9 +82,11 @@ def svm_loss_vectorized(W, X, y, reg):
     # result in loss.                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
-
+    XW = X.dot(W)
+    W_y = W[:,y]
+    v = X.dot(W_y).diagonal().reshape([1, X.shape[0]])
+    margin = XW.T - v + 1
+    loss = np.sum(margin.T)/X.shape[0] + reg*np.sum(W*W)
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     #############################################################################
