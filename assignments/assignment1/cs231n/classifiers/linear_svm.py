b7d2bde80d53a6127f939a5822c37db6e17e6920
Alex Lin <349461694@qq.com>
2022-07-10 22:55:34 -0400

    up

--- assignments/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments/assignment1/cs231n/classifiers/linear_svm.py
@@ -23,7 +23,7 @@ def svm_loss_naive(W, X, y, reg):
     - gradient with respect to weights W; an array of same shape as W
     """
     dW = np.zeros(W.shape)  # initialize the gradient as zero
-
+   
     # compute the loss and the gradient
     num_classes = W.shape[1]
     num_train = X.shape[0]
@@ -34,7 +34,7 @@ def svm_loss_naive(W, X, y, reg):
         for j in range(num_classes):
             if j == y[i]:
                 continue
-            margin = scores[j] - correct_class_score + 1  # note delta = 1
+            margin = scores[j] - correct_class_score + 1  # note delta = 1 
             if margin > 0:
                 loss += margin
 
@@ -54,8 +54,7 @@ def svm_loss_naive(W, X, y, reg):
     # code above to compute the gradient.                                       #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+                
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
