44b819765539871a76471c8ef833d8978eff02c7
Alex Lin <349461694@qq.com>
2022-07-12 22:13:10 -0400

    relu forward and backward

--- assignments/assignment1/cs231n/layers.py
+++ assignments/assignment1/cs231n/layers.py
@@ -91,7 +91,7 @@ def relu_forward(x):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    out = np.maximum(0, x)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -118,7 +118,7 @@ def relu_backward(dout, cache):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    dx = dout*(np.where(x>0, 1, 0))
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
